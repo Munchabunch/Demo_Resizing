3c444a85106220ce883f97034e0e1697f55dc26a
Michael C. Adams <trailodex@yahoo.com>
2018-08-27 13:12:07 -0400

    Add files via upload

--- Demo_Resizing/frm_Demo_Resizing.Designer.vb
+++ Demo_Resizing/frm_Demo_Resizing.Designer.vb
@@ -27,11 +27,19 @@ Partial Class frm_Demo_Resizing
         Me.btn_04 = New System.Windows.Forms.Button()
         Me.btn_03 = New System.Windows.Forms.Button()
         Me.btn_05 = New System.Windows.Forms.Button()
+        Me.oSplitContainer_01 = New System.Windows.Forms.SplitContainer()
+        Me.btn_11 = New System.Windows.Forms.Button()
+        Me.btn_12 = New System.Windows.Forms.Button()
+        Me.btn_13 = New System.Windows.Forms.Button()
+        CType(Me.oSplitContainer_01, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.oSplitContainer_01.Panel1.SuspendLayout()
+        Me.oSplitContainer_01.Panel2.SuspendLayout()
+        Me.oSplitContainer_01.SuspendLayout()
         Me.SuspendLayout()
         '
         'btn_01
         '
-        Me.btn_01.Location = New System.Drawing.Point(0, 80)
+        Me.btn_01.Location = New System.Drawing.Point(0, 44)
         Me.btn_01.Name = "btn_01"
         Me.btn_01.Size = New System.Drawing.Size(125, 25)
         Me.btn_01.TabIndex = 0
@@ -40,7 +48,7 @@ Partial Class frm_Demo_Resizing
         '
         'btn_02
         '
-        Me.btn_02.Location = New System.Drawing.Point(130, 80)
+        Me.btn_02.Location = New System.Drawing.Point(130, 44)
         Me.btn_02.Name = "btn_02"
         Me.btn_02.Size = New System.Drawing.Size(125, 25)
         Me.btn_02.TabIndex = 1
@@ -49,7 +57,7 @@ Partial Class frm_Demo_Resizing
         '
         'btn_04
         '
-        Me.btn_04.Location = New System.Drawing.Point(85, 118)
+        Me.btn_04.Location = New System.Drawing.Point(85, 82)
         Me.btn_04.Name = "btn_04"
         Me.btn_04.Size = New System.Drawing.Size(80, 25)
         Me.btn_04.TabIndex = 4
@@ -58,7 +66,7 @@ Partial Class frm_Demo_Resizing
         '
         'btn_03
         '
-        Me.btn_03.Location = New System.Drawing.Point(0, 118)
+        Me.btn_03.Location = New System.Drawing.Point(0, 82)
         Me.btn_03.Name = "btn_03"
         Me.btn_03.Size = New System.Drawing.Size(80, 25)
         Me.btn_03.TabIndex = 3
@@ -67,25 +75,77 @@ Partial Class frm_Demo_Resizing
         '
         'btn_05
         '
-        Me.btn_05.Location = New System.Drawing.Point(170, 118)
+        Me.btn_05.Location = New System.Drawing.Point(170, 82)
         Me.btn_05.Name = "btn_05"
         Me.btn_05.Size = New System.Drawing.Size(80, 25)
         Me.btn_05.TabIndex = 5
         Me.btn_05.Text = "05"
         Me.btn_05.UseVisualStyleBackColor = True
         '
+        'oSplitContainer_01
+        '
+        Me.oSplitContainer_01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.oSplitContainer_01.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.oSplitContainer_01.Location = New System.Drawing.Point(0, 0)
+        Me.oSplitContainer_01.Name = "oSplitContainer_01"
+        Me.oSplitContainer_01.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'oSplitContainer_01.Panel1
+        '
+        Me.oSplitContainer_01.Panel1.Controls.Add(Me.btn_02)
+        Me.oSplitContainer_01.Panel1.Controls.Add(Me.btn_05)
+        Me.oSplitContainer_01.Panel1.Controls.Add(Me.btn_01)
+        Me.oSplitContainer_01.Panel1.Controls.Add(Me.btn_04)
+        Me.oSplitContainer_01.Panel1.Controls.Add(Me.btn_03)
+        '
+        'oSplitContainer_01.Panel2
+        '
+        Me.oSplitContainer_01.Panel2.Controls.Add(Me.btn_13)
+        Me.oSplitContainer_01.Panel2.Controls.Add(Me.btn_12)
+        Me.oSplitContainer_01.Panel2.Controls.Add(Me.btn_11)
+        Me.oSplitContainer_01.Size = New System.Drawing.Size(296, 292)
+        Me.oSplitContainer_01.SplitterDistance = 146
+        Me.oSplitContainer_01.TabIndex = 6
+        '
+        'btn_11
+        '
+        Me.btn_11.Location = New System.Drawing.Point(0, 0)
+        Me.btn_11.Name = "btn_11"
+        Me.btn_11.Size = New System.Drawing.Size(75, 25)
+        Me.btn_11.TabIndex = 0
+        Me.btn_11.Text = "11"
+        Me.btn_11.UseVisualStyleBackColor = True
+        '
+        'btn_12
+        '
+        Me.btn_12.Location = New System.Drawing.Point(0, 30)
+        Me.btn_12.Name = "btn_12"
+        Me.btn_12.Size = New System.Drawing.Size(75, 25)
+        Me.btn_12.TabIndex = 1
+        Me.btn_12.Text = "12"
+        Me.btn_12.UseVisualStyleBackColor = True
+        '
+        'btn_13
+        '
+        Me.btn_13.Location = New System.Drawing.Point(0, 60)
+        Me.btn_13.Name = "btn_13"
+        Me.btn_13.Size = New System.Drawing.Size(75, 25)
+        Me.btn_13.TabIndex = 2
+        Me.btn_13.Text = "13"
+        Me.btn_13.UseVisualStyleBackColor = True
+        '
         'frm_Demo_Resizing
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(283, 193)
-        Me.Controls.Add(Me.btn_05)
-        Me.Controls.Add(Me.btn_04)
-        Me.Controls.Add(Me.btn_03)
-        Me.Controls.Add(Me.btn_02)
-        Me.Controls.Add(Me.btn_01)
+        Me.ClientSize = New System.Drawing.Size(296, 292)
+        Me.Controls.Add(Me.oSplitContainer_01)
         Me.Name = "frm_Demo_Resizing"
         Me.Text = "Demo: Resizing"
+        Me.oSplitContainer_01.Panel1.ResumeLayout(False)
+        Me.oSplitContainer_01.Panel2.ResumeLayout(False)
+        CType(Me.oSplitContainer_01, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.oSplitContainer_01.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -95,4 +155,8 @@ Partial Class frm_Demo_Resizing
     Friend WithEvents btn_04 As Button
     Friend WithEvents btn_03 As Button
     Friend WithEvents btn_05 As Button
+    Friend WithEvents oSplitContainer_01 As SplitContainer
+    Friend WithEvents btn_13 As Button
+    Friend WithEvents btn_12 As Button
+    Friend WithEvents btn_11 As Button
 End Class
--- Demo_Resizing/frm_Demo_Resizing.vb
+++ Demo_Resizing/frm_Demo_Resizing.vb
@@ -1,16 +1,21 @@
 ﻿Public Class frm_Demo_Resizing
 
     Private Sub frm_Demo_Resizing_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ResizeUniformly({btn_01, btn_02}, Me.ClientSize.Width.ToString)
-        ResizeUniformly({btn_03, btn_04, btn_05}, Me.ClientSize.Width.ToString)
+        ResizeUniformly_Horizontal({btn_01, btn_02}, Me.ClientSize.Width)
+        ResizeUniformly_Horizontal({btn_03, btn_04, btn_05}, Me.ClientSize.Width)
+        ResizeUniformly_Vertical({btn_11, btn_12, btn_13}, oSplitContainer_01.Panel2.Height)
     End Sub
 
     Private Sub frm_Demo_Resizing_ResizeEnd(sender As Object, e As EventArgs) Handles MyBase.ResizeEnd
-        ResizeUniformly({btn_01, btn_02}, Me.ClientSize.Width.ToString)
-        ResizeUniformly({btn_03, btn_04, btn_05}, Me.ClientSize.Width.ToString)
+        ResizeUniformly_Horizontal({btn_01, btn_02}, Me.ClientSize.Width)
+        ResizeUniformly_Horizontal({btn_03, btn_04, btn_05}, Me.ClientSize.Width)
     End Sub
 
-    Private Sub ResizeUniformly(ctrls As Control(), TotalWidth_New As Integer)
+    Private Sub oSplitContainer_01_SplitterMoved(sender As Object, e As SplitterEventArgs) Handles oSplitContainer_01.SplitterMoved
+        ResizeUniformly_Vertical({btn_11, btn_12, btn_13}, oSplitContainer_01.Panel2.Height)
+    End Sub
+
+    Private Sub ResizeUniformly_Horizontal(ctrls As Control(), TotalWidth_New As Integer)
 
         Dim ctrl_Working As Control
         Dim ctrl_Previous As Control
@@ -38,6 +43,39 @@
             ctrl_Working = ctrls(i_CtrlNum)
             ctrl_Working.Width = CtrlWidth_New
             ctrl_Working.Left = ctrl_Previous.Right + GutterWidth
+            ctrl_Previous = ctrl_Working
+        Next i_CtrlNum
+
+    End Sub
+
+    Private Sub ResizeUniformly_Vertical(ctrls As Control(), TotalHeight_New As Integer)
+
+        Dim ctrl_Working As Control
+        Dim ctrl_Previous As Control
+        Dim CtrlHeight_Old As Integer
+        Dim CtrlHeight_New As Integer
+        Dim GutterHeight As Integer
+        Dim CombinedGutterHeight As Integer
+        Dim TotalHeight_Old As Integer
+        Dim i_CtrlNum As Integer
+
+
+        CtrlHeight_Old = ctrls(0).Height
+        GutterHeight = ctrls(1).Top - ctrls(0).Bottom
+        CombinedGutterHeight = GutterHeight * (ctrls.Length - 1)
+        TotalHeight_Old = (CtrlHeight_Old * ctrls.Length) + CombinedGutterHeight
+
+        CtrlHeight_New = (TotalHeight_New - CombinedGutterHeight) \ ctrls.Length
+
+        '// Resize the first control:
+        ctrls(0).Height = CtrlHeight_New
+        ctrl_Previous = ctrls(0)
+
+        '// Move and resize the rest of the controls:
+        For i_CtrlNum = 1 To ctrls.Length - 1
+            ctrl_Working = ctrls(i_CtrlNum)
+            ctrl_Working.Height = CtrlHeight_New
+            ctrl_Working.Top = ctrl_Previous.Bottom + GutterHeight
             ctrl_Previous = ctrl_Working
         Next i_CtrlNum
 
